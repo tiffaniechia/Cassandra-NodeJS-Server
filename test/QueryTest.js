b467884758bd6f4f8e46188481320b783c92cb5a
tiffaniechia <tiffaniekyrachia@gmail.com>
2015-08-25 16:04:42 +0800

    created queryservice helper

--- test/QueryTest.js
+++ test/QueryTest.js
@@ -1,13 +1,31 @@
 var sinon = require("sinon");
 var chai = require('chai');
+var sinonChai = require("sinon-chai");
 chai.should();
+chai.use(sinonChai);
+
 var Query = require('../Query.js');
+var queryService = require('../QueryService.js');
+//var app = require();
 
 describe('Query', function() {
     describe('#init', function () {
         it('initialize with query', function () {
             var query = new Query('blank space');
             query.searchTerm.should.equal('blank space');
+
         });
+    });
+
+    describe('#isValidLyric', function () {
+        it('should call Service with search', function () {
+            sinon.spy(queryService, 'getQueryValidity');
+            var query = new Query('blank space');
+            query.search();
+            queryService.getQueryValidity.should.have.been.calledWith('blank space');
+
+        });
+
+
     });
 });
