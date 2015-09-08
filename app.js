e1be3025604ec0595aee7e1d4654d2f4bf65629d
tiffaniechia <tiffaniekyrachia@gmail.com>
2015-09-08 17:46:45 +0100

    moved cassandra setup to run script

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-//CASSANDRA SETUP
 var express = require('express');
 var bodyParser = require('body-parser');
 var cassandra = require('cassandra-driver');
-var async = require('async');
 var app = express();
 app.use(bodyParser.json());
 
 var client = new cassandra.Client({contactPoints: ['127.0.0.1']});
+
 client.connect(function (err, result) {
     console.log('Connected.');
+});
+
+var server = app.listen(3000, function () {
+    console.log('Listening on port %d', server.address().port);
 });
 
 app.get('/metadata', function (req, res) {
@@ -17,70 +20,12 @@ app.get('/metadata', function (req, res) {
     }));
 });
 
-app.post('/keyspace', function (req, res) {
-    client.execute("CREATE KEYSPACE IF NOT EXISTS cassandra_base WITH replication " +
-        "= {'class' : 'SimpleStrategy', 'replication_factor' : 3};",
-        afterExecution('Error: ', 'Keyspace created.', res));
-});
-
-function afterExecution(errorMessage, successMessage, res) {
-    return function (err) {
-        if (err) {
-            console.log(errorMessage);
-            return res.json(errorMessage);
-        } else {
-            res.json(successMessage);
-        }
-    }
-}
-
-var server = app.listen(3000, function () {
-    console.log('Listening on port %d', server.address().port);
-});
-
-
-app.post('/tables', function (req, res) {
-    async.parallel([
-            function (next) {
-                client.execute('CREATE TABLE IF NOT EXISTS cassandra_base.users (' +
-                    'username text PRIMARY KEY,' +
-                    'password text,' +
-                    ');',
-                    next);
-            },
-            function (next) {
-                client.execute('CREATE TABLE IF NOT EXISTS cassandra_base.shouts (' +
-                    'shout_id uuid PRIMARY KEY,' +
-                    'username text,' +
-                    'body text,' +
-                    ');',
-                    next);
-            },
-
-            function (next) {
-                client.execute('CREATE TABLE IF NOT EXISTS cassandra_base.usershouts (' +
-                    'username text,' +
-                    'body text,' +
-                    'shout_id uuid,' +
-                    'PRIMARY KEY (shout_id, username)' +
-                    ');',
-                    next);
-            }
-        ],
-        afterExecution('Error: ', 'Tables created.', res));
-});
-
-
 app.get('/users', function (req, res) {
-
     client.execute('SELECT * FROM cassandra_base.users', [], function (err, result) {
         if (err) {
             res.status(404).send({msg: err});
         } else {
-           
             res.json(result);
         }
     });
 });
-
-
